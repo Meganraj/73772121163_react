df69821283202929dfb4a74e2dc42d93e1fef234
Meganraj <meganrajmegan.com>
2023-12-09 13:24:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MY DETAILS</h1>
+        
+        <h2>
+          NAME : MEGANRAJ S
+        </h2>
+        <h2>DEPARTMENT : INFORMATION TECHNOLOGY</h2>
+        <h2>YEAR : 3rd YEAR</h2>
+        <h2>REGISTER_NUMBER : 73772121163</h2>
+        <h2>AREA OF INTREST : FULL STACK DEVELOPMENT</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
